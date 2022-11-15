3303021ff4fa316ae78a6cfb412e76933b16b578
Cam Nugent <camnugent@gmail.com>
2022-11-15 09:35:20 -0400

    docs for new functions

--- scripts/01_make_snp_flank_fasta.py
+++ scripts/01_make_snp_flank_fasta.py
@@ -1,9 +1,12 @@
+import gc
 import pandas as pd
 from seqio import read_fasta, write_fasta
 
 
 #header = ">CM003279.1 Salmo salar isolate Sally breed double haploid chromosome ssa01, whole genome shotgun sequence"
 def subset_v2_chr_header(header):
+    """Take a long form header from the chr 2 genome 
+        and subset just the ssa style chr designation."""
     front = header.split(',')[0]
     chrom = front.split("haploid chromosome ")[-1]
 
@@ -15,9 +18,10 @@ def subset_v2_chr_header(header):
 
 
 def build_subset_dict(genome_dict, merged_scaffold_name= "ssa30"):
+    """Take a dictonary of fasta entries, simplify the chromosome names, and 
+        merge all of the unplaced scaffolds (in order) to make an additional pseudochromosome"""
     subset_dict = {}
     for k, v in genome_dict.items():
-
         if "chromosome ssa" in k:
             new_k = subset_v2_chr_header(k)
             subset_dict[new_k] = v
@@ -28,7 +32,7 @@ def build_subset_dict(genome_dict, merged_scaffold_name= "ssa30"):
         else:
             subset_dict[merged_scaffold_name] = v
 
-
+    return subset_dict
 
 
 if __name__ == "__main__":
@@ -42,4 +46,7 @@ if __name__ == "__main__":
     raw_v2_genome_data = read_fasta(GENOME_INPUT)
 
     v2_genome_cleaned = build_subset_dict(raw_v2_genome_data)
+    raw_v2_genome_data = []
+    gc.collect()
 
+    
