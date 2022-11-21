4b4ec5eddef6b59054cf39af7586d38426cd026b
Cam Nugent <camnugent@gmail.com>
2022-11-21 14:29:45 -0400

    completed the matching to old info

--- scripts/05_parse_snp-placer_outputs.py
+++ scripts/05_parse_snp-placer_outputs.py
@@ -48,12 +48,25 @@ if __name__ == "__main__":
     map_file_addendum = unmapped_snps[["CHR", "SNP", "ZERO", "POS"]]
 
     merged_map_df = pd.concat([map_file_output, map_file_addendum])
+    merged_map_df = merged_map_df.drop_duplicates()
     #double check for duplicate values onces all together, remove these and move 
     vc_gte_1 = merged_map_df["SNP"].value_counts()
     vc_gte_1 = vc_gte_1[vc_gte_1>1]
-    vc_gte_1
+    dup_snps = set(vc_gte_1.index)
+    map_file_nodup = merged_map_df[[x not in dup_snps for x in merged_map_df["SNP"].values ]]
 
+    map_file_dups = merged_map_df[[x in dup_snps for x in merged_map_df["SNP"].values ]]
+    map_file_dups["CHR"] = 30
+    map_file_dups["POS"] = 30
+    map_file_dups = map_file_dups.drop_duplicates()
+    map_file_dups.reset_index(inplace=True, drop = True)
+    map_file_dups["POS"] = map_file_dups.index + 220000
 
+    new_map_file_cleaned = pd.concat([map_file_nodup, map_file_dups], ignore_index=True)
+
+    assert len(new_map_file_cleaned) == 220000 #all aboard?
+
+    new_map_file_cleaned.to_csv("../data/Ssa220K_v3_Genome.map", sep = "\t", index = False)
 
     """ read  in the old map file, do a comparison of the new snp positions to the old
     
@@ -61,3 +74,32 @@ if __name__ == "__main__":
 
     # MAP FORMAT:
     # CHR   SNP ZERO    POS
+    #old map in
+    old_map = pd.read_csv(OLD_LOCATIONS, sep = "\t")
+    old_map.CHR[old_map.CHR.isna()] = 30.0
+    old_map["CHR"] = old_map["CHR"].apply(lambda x: int(x))
+
+    #convert new and old maps to dicts and record differences
+
+    new_map_dict = {}
+    for i, row in new_map_file_cleaned.iterrows():
+        new_map_dict[row["SNP"]] = {"POS": row["POS"], "CHR" : row["CHR"]}
+
+    old_map_dict = {}
+    for i, row in old_map.iterrows():
+        old_map_dict[row["SNP"]] = {"POS": row["POS"], "CHR" : row["CHR"]}
+
+    total = len(new_map_dict)
+    matches = 0
+    non_matches = []
+
+    for k, new_v in new_map_dict.items():
+        old_v = old_map_dict[k]
+        if new_v["CHR"] == old_v["CHR"]:
+            matches +=1
+        else:
+            non_matches.append({"new_CHR" : new_v["CHR"], 
+                                 "old_CHR" : old_v["CHR"]})
+
+    matches / total #96.85% go to the same chromosome
+    #this seems reasonable
